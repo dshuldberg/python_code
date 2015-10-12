c3f9537e7e27ec43b4e2353e5d76d5b4ae09d3f5
dshuldberg <dshuldberg019@rsdmo.org>
2015-10-12 18:34:45 -0500

    Update python_1.py

--- python_1.py
+++ python_1.py
@@ -1,3 +1,3 @@
 name = raw_input("What is your name?")
 new_name = name[1:len(name)]
-print "%s %s bo b%s, banana-fana fo f%s, me my mo m%s, %s" (name, name, new_name, new_name, new_name, name)
+print "%s %s bo b%s, banana-fana fo f%s, me my mo m%s, %s" % (name, name, new_name, new_name, new_name, name)
